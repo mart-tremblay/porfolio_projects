6a1eec670549a65481591cfc6fc286d0437402ab
Martin Tremblay <martin.tremblay@pme-ti.com>
2024-08-07 16:30:01 -0400

    Message components added (subjects, verbs, nouns).

---
+++ messageGenerator.js
@@ -0,0 +1,36 @@
+// Message Generator apps
+const messageGenerator = {
+    subject: ["L'attaquant",
+              "Le défenseur",
+              "Le gardien",
+              "Le joueur de tennis",
+              "Le coureur",
+              "Le nageur",
+              "Le boxeur",
+              "L'arbitre",
+              "L'entraîneur",
+              "L'équipe",],
+    verb: ["marque",
+           "défend",
+           "bloque",
+           "sert",
+           "court",
+           "nage",
+           "frappe",
+           "siffle",
+           "entraîne",
+           "gagne",],
+    noun: ["un but",
+           "une passe",
+           "une frappe",
+           "un ace",
+           "un sprint",
+           "une longueur",
+           "un coup",
+           "une faute",
+           "un exercice",
+           "le match",],
+
+
+}
+
